14d120b3865afdcf106a7ec21767b6169dfdb16e
Kylie Callaway <majorkylie404@gmail.com>
2021-11-05 19:08:09 -0600

    updated forum/home pages and added bootstrap the index.html

---
+++ src/views/Forum.vue
@@ -0,0 +1,68 @@
+<template>
+<div class="title-container">
+  <div class="title">
+    <h1>Forum</h1>
+    <h2>Which game is your favorite?<h2>
+  </div>
+<div class="Reviews-Container">
+    <div class="forum-item">
+      <h2 id="person">Moira Rose</h2>
+      <p id="reviews">"This place is almost charming. Very rustic cottage... I was half expecting early Unabomber."</p>
+    </div>
+    <div class="forum-item">
+      <h2 id="person">Johnny Rose</h2>
+      <p id="reviews">"Let me explain something about business. It's a dance. And sometimes you lead. And sometimes you follow."</p>
+    </div>
+    <div class="forum-item">
+      <h2 id="person">David Rose</h2>
+      <p id="reviews">"I would hardly call myself and expert on this subject, and by subject, I mean genuine human emotion, so I am just going to tell you what I know."</p>
+    </div>
+    <div class="forum-item">
+      <h2 id="person">Alexis Rose</h2>
+      <p id="reviews">"Every now and again, spending like a little bit of meny on something really special... it might not buy happiness, but it can definitely help make you smile."</p>
+    </div>
+    <div class="forum-item">
+      <h2 id="person">Stevie Bud</h2>
+      <p id="reviews">"Best Wishes and Warmest Regards!"</p>
+    </div>
+</div>
+</template>
+
+<style>
+/*Reviews Page*/
+
+.forum-item {
+  background-color: #D3FAD7;
+  display:flex;
+  flex-direction: column;
+  flex-wrap: wrap;
+  margin-bottom: 20px;
+  padding: 20px 20px 20px 20px;
+
+}
+.forum-Container{
+  margin-top: 50px;
+  margin-bottom: 50px;
+}
+
+#reviews {
+  font-style: italic;
+  color: #22577A;
+
+
+  /*Title*/
+
+  .title {
+    display: flex;
+    text-align: center;
+    margin-top: 50px;
+    margin-bottom: 50px;
+    flex-direction: column;
+  }
+  .title-container {
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+  }
+
+</style>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,8 +1,17 @@
 <template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+<!--Intro Content-->
+<div class="Intro-content-container">
+  <div class="Intro-content">
+    <h1>Battle of the Boardgames!</h1>
+    <a href="/pages/forum.html" style="color: #22577A" id="subtitle"><em>Which is your favorite?</em></a>
   </div>
+</div>
+<!--Photo Bar-->
+<div class="photo-container-home">
+  <div class="photo-home"><a class="anch-link" accesskey=""href="https://www.amazon.com/Asmodee-SEV07-7-Wonders-Duel/dp/B014DMSTXK"><img class="photosize" src="images/7Wondersduelimage.jpg" alt="Picture of 7 Wonders Duel Boardgame"></a></div>
+  <div class="photo-home"><a class="anch-link" href="https://www.amazon.com/Rio-Grande-Games-Bohnanza-Duel/dp/B071RVLJNZ"><img class="photosize" src="images/Bohnanzaimage.jpg" alt="Picture of Bohnanza Boardgame"></a></div>
+  <div class="photo-home"><a class="anch-link" href="https://www.amazon.com/Catan-Studios-cantan2017/dp/B00U26V4VQ"><img class="photosize" src="images/Settlerpicture.jpg" alt="Picture of Setters of Catan Boardgame"></a></div>
+</div>
 </template>
 
 <script>
@@ -16,3 +25,48 @@ export default {
   }
 }
 </script>
+
+<style>
+/*Home Page*/
+.Intro-content-container{
+  display:flex;
+  margin: 25px 25px 25px 25px;
+  justify-content: center;
+}
+
+.photo-container-home, .photo-container-portfolio, .pricing-container {
+  display: flex;
+  flex-direction: row;
+  justify-content: center;
+  flex-wrap: wrap;
+}
+
+.photo-home{
+  margin: 1rem 1rem 1rem 1rem;
+  justify-content: center;
+  margin: 50px;
+}
+
+.photo{
+  margin: 2rem;
+  display: flex;
+  justify-content: center;
+  padding: 1rem;
+}
+
+/*Title*/
+
+.title {
+  display: flex;
+  text-align: center;
+  margin-top: 50px;
+  margin-bottom: 50px;
+  flex-direction: column;
+}
+.title-container {
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+}
+
+</style>
