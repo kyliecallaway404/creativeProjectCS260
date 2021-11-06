54af3796d062e251363ba4ef9257ec739ceee233
RefinedSnack <71367210+RefinedSnack@users.noreply.github.com>
2021-11-06 14:02:25 -0600

    Added the new files

--- src/router/index.js
+++ src/router/index.js
@@ -2,6 +2,9 @@ import Vue from 'vue'
 import VueRouter from 'vue-router'
 import Home from '../views/Home.vue'
 import Forum from '../views/Forum.vue'
+import Catan from '../views/Catan.vue'
+import SevenWonders from '../views/SevenWonders.vue'
+import Bohnanza from '../views/Bohnanza.vue'
 
 Vue.use(VueRouter)
 
@@ -15,6 +18,21 @@ const routes = [
     path: '/Forum',
     name: 'Forum',
     component: Forum
+  },
+  {
+    path: '/Catan',
+    name: 'Catan',
+    component: Catan
+  },
+  {
+    path: '/SevenWonders',
+    name: 'SevenWonders',
+    component: SevenWonders
+  },
+  {
+    path: '/Bohnanza',
+    name: 'Bohnanza',
+    component: Bohnanza
   }
 ]
 
